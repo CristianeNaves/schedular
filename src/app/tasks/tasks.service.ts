becaee620594b6b1e4f981889e62aaeeb87d1fae
Cristiane Naves <cristianenavescardoso09@gmail.com>
2019-02-17 12:05:16 -0300

    adding create button

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -11,7 +11,12 @@ export class TasksService {
   private tasks: Task[] = [
     {id: 1, title: '', description: '', completed: false, deadline: '08/02/1996'},
     {id: 2, title: '', description: '', completed: false, deadline: '08/02/1996'},
-    {id: 3, title: '', description: '', completed: false, deadline: '08/02/1996'}
+    {id: 3, title: '', description: '', completed: false, deadline: '08/02/1996'},
+    {id: 4, title: '', description: '', completed: false, deadline: '08/02/1996'},
+    {id: 5, title: '', description: '', completed: false, deadline: '08/02/1996'},
+    {id: 6, title: '', description: '', completed: false, deadline: '08/02/1996'},
+    {id: 7, title: '', description: '', completed: false, deadline: '08/02/1996'},
+    {id: 8, title: '', description: '', completed: false, deadline: '08/02/1996'}
   ];
 
   constructor() { }
