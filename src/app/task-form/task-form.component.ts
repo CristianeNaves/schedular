5e675953fa162bcfd8201cdc2e9977d6edc8c526
Cristiane Naves <cristianenavescardoso09@gmail.com>
2019-02-22 11:13:11 -0300

    Adding validation to fields

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -14,6 +14,8 @@ export class TaskFormComponent implements OnInit {
   private task: Task = {Title: null, Description: null, Completed: null, Deadline: null, UserId: "07ae89bd848e414ba160afd6330cac"};
   private sub: Subscription;
   private subTask: Subscription;
+  private subUpdate: Subscription;
+  private subCreate: Subscription;
   private showDeleteScreen: boolean = false;
 
   constructor(
@@ -28,52 +30,45 @@ export class TaskFormComponent implements OnInit {
         let id = params["id"];
         if (id) { 
           this.subTask = this.tasksService.getTask(id).subscribe(
-          data => { this.task = data, 
-            this.tasksService.configInputParams(this.task) 
-          console.log(this.task)},
-          error => { 
+          (data) => { this.task = data, 
+            this.tasksService.configInputParams(this.task) },
+          (error) => { 
             this.router.navigate([''])
-          }
-          );
+          });
         } 
       });
   }
-
-  ngOnDestroy() {
-    if (this.sub) {
-      this.sub.unsubscribe(); 
-    }
-    if (this.subTask){
-      this.subTask.unsubscribe();
-    }
-  }
-
+  
   onSubmit(form){
-    //arrumar o completed e o deadline
     if (this.task.Title) {
       if (this.task.Id == undefined || this.task.Id == null) {
-        console.log(this.task);
-        this.tasksService.createTask(this.task).subscribe(
-          (val) => { console.log(val) },
-          (error) => { console.log(error) }
-        );
+        this.subCreate = this.tasksService.createTask(this.task).subscribe(
+          (response) => console.log(response),
+          (error) => console.log(error)
+          );
       } else {
-        this.tasksService.updateTask(this.task).subscribe(
-          (val) => { console.log(val) },
-          (error) => { console.log(error) }
-        );
+        this.subUpdate = this.tasksService.updateTask(this.task).subscribe(
+          (response) => console.log(response),
+          (error) => console.log(error)
+          );
+        }
       }
-    }
-    console.log("aqui");  //nao entrar no submit se o title nao estive preenchido
     this.router.navigate(['']);
   }
-
+    
   deleteTask() {
     this.tasksService.deleteTask(this.task).subscribe(
-      (val) => { console.log(val) },
-      (error) => { console.log(error) }
+      (response) => console.log(response),
+      (error) => console.log(error)
     );
     this.router.navigate(['']);  //nao vai estar atualizado
   }
-
+      
+  ngOnDestroy() {
+    if (this.sub) this.sub.unsubscribe(); 
+    if (this.subTask) this.subTask.unsubscribe();
+    if (this.subCreate) this.subCreate.unsubscribe();
+    if (this.subUpdate) this.subUpdate.unsubscribe();
+  }
 }
+    
